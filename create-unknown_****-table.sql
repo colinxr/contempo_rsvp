1c36d5caadcfc90fe3602c03748346913ac1bd2b
colinxr <colinxr@gmail.com>
2017-09-14 13:04:04 -0400

    adds sql table templates to repo

---
+++ create-unknown_****-table.sql
@@ -0,0 +1,29 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Table structure for table `unknown_****`
+--
+
+--
+-- Table structure for table `unknown_****`
+--
+
+CREATE TABLE IF NOT EXISTS `unknown_****` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `firstName` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `lastName` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `postal` varchar(7) COLLATE utf8_unicode_ci NOT NULL,
+  `PLUSONE` int(11) NOT NULL DEFAULT '0',
+  `guestName` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `guestEmail` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  UNIQUE KEY `ID` (`ID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=143 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
