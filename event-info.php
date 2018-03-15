da140e98797e4352b4269682b9d0487e3777e1df
colinxr <colinxr@gmail.com>
2018-03-15 10:00:36 -0400

    update copy

--- event-info.php
+++ event-info.php
@@ -9,8 +9,8 @@
 <div class="details-large">
   <h2>Contempo RSVP OOP Rebuild System</h2>
 
-  <h4>Warning: This is the OOP Version </h4>
-  <h4>Make Sure you dont' want to be working on the main branch</h4>
+  <h4>Refer to Readme for instructions</h4>
+  <h4>Insert event info here</h4>
 
   <h4>Address Info</h4>
   <h5>Numbers</h5>
